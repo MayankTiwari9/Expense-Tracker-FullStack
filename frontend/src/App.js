e2ed76497012f12544ffce1f8c1564df237dfed2
MayankTiwari9 <mt1027319@gmail.com>
2024-05-29 23:14:22 +0530

    Log In component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import SingInForm from "./Components/SignInForm/SingInForm";
 import SignUpForm from "./Components/SignUpForm/SignUpForm";
 
 function App() {
   return (
     <div className="App">
-      <SignUpForm/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUpForm/>}/>
+        <Route path="/signin" element={<SingInForm/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/Components/SignInForm/SingInForm.js
+++ frontend/src/Components/SignInForm/SingInForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const SignUpForm = () => {
-  const [name, setName] = useState("");
+const SingInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -10,17 +9,15 @@ const SignUpForm = () => {
     e.preventDefault();
 
     const formData = {
-      name,
       email,
       password,
     };
 
     axios
-      .post("http://localhost:3001/users/signup", formData)
+      .post("http://localhost:3001/users/signin", formData)
       .then((res) => {
         console.log(res);
         alert(res.data.message);
-        setName("");
         setEmail("");
         setPassword("");
       })
@@ -35,30 +32,10 @@ const SignUpForm = () => {
     <form onSubmit={formSubmitHandler}>
       <div className="border border-gray-900/10 mt-10 p-6 w-1/4 max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-4xl mx-auto flex flex-col justify-center">
         <h1 className="text-2xl font-semibold leading-7 text-gray-900 mx-auto">
-          Sign Up
+          log In
         </h1>
 
         <div className="mt-8 grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-4">
-          <div className="col-span-1 sm:col-span-2">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Name
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="name"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                required
-              />
-            </div>
-          </div>
-
           <div className="col-span-1 sm:col-span-2">
             <label
               htmlFor="email"
@@ -68,7 +45,7 @@ const SignUpForm = () => {
             </label>
             <div className="mt-2">
               <input
-                type="email"
+                type="text"
                 name="email"
                 id="email"
                 value={email}
@@ -103,11 +80,11 @@ const SignUpForm = () => {
           type="submit"
           className="mt-6 w-full sm:w-auto sm:self-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Sign Up
+          Log In
         </button>
       </div>
     </form>
   );
 };
 
-export default SignUpForm;
+export default SingInForm;
